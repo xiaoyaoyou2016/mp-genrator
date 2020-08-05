45cad740b8336c5e4d59d9c29e840c1c2dea3658
zlb <xiaoyaoyou_007@126.com>
2020-08-05 22:55:33 +0800

    init

---
+++ src/main/java/com/cujia/mpgenrator/MpGenratorApplication.java
@@ -0,0 +1,13 @@
+package com.cujia.mpgenrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpGenratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpGenratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cujia/mpgenrator/MpGenratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cujia.mpgenrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpGenratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
